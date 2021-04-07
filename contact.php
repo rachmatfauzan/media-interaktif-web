0924d587471b01f5961b684e40871d34d26c0bd1
Rachmat Fauzan <rachmatfauzan07@gmail.com>
2021-04-07 13:01:31 +0700

    membuat script pada app script google dengan spreadsheet

--- contact.php
+++ contact.php
@@ -9,15 +9,14 @@
     <link rel="icon" href="images/TFME.jpg">
     <link rel="stylesheet" href="css/style.css">
     <!-- <link rel="stylesheet" href="fa/css/all.min.css"> -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/contact.css">
 </head>
 
 <body>
     <!-- Image and text -->
-    <nav class="navbar ">
+    <nav class="navbar p-1">
         <a class="navbar-brand text-white" href="#">
             <img src="images/TFME.jpg" width="150" height="80" class="d-inline-block align-top" alt="">
         </a>
@@ -27,31 +26,75 @@
     <div class="container d-flex flex-column">
 
         <h5 style="text-align: center;">Contact Us</h5>
+        <div class="alert d-none alert-success alert-dismissible fade show" role="alert">
+            <strong>Congratulation</strong> Thank you for your comment,we'll read your comment
+        </div>
 
-        <form>
+        <form name="tfme-contact">
             <div class="form-group">
                 <label for="exampleInputEmail1">Full Name</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Enter name" required>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="fullname"
+                    aria-describedby="emailHelp" placeholder="Enter name" required>
             </div>
-            <div class="form-group">
+            <div class="form-group mt-2">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Enter email" required>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1"
+                    aria-describedby="emailHelp" placeholder="Enter email" required>
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                     else.</small>
             </div>
 
-            <div class="form-group">
+            <div class="form-group mt-2">
                 <label for="exampleInputEmail1">Comment</label>
-                <textarea type="text" class="form-control" rows="5"
-                    placeholder="Your comment..." required></textarea>
+                <textarea type="text" class="form-control" rows="5" name="comment" placeholder="Your comment..."
+                    required></textarea>
             </div>
 
-            <button type="submit" class="btn btn-warning">Submit</button>
+            <button type="submit" class="btn btn-warning mt-4 btn-kirim">Submit</button>
+
+            <button class="btn btn-warning btn-loading mt-3 d-none" type="button" disabled>
+                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                Loading...
+            </button>
         </form>
     </div>
 
+
+    <script>
+        const scriptURL =
+            'https://script.google.com/macros/s/AKfycbz-tslnidQPIEEojRHRFep4CP5cIw2VuCW0Wxrno8fadc7YO2rtElTbu1dcR9opbxRp/exec'
+        const form = document.forms['tfme-contact']
+
+        const btnKirim = document.querySelector('.btn-kirim');
+        const btnLoading = document.querySelector('.btn-loading');
+        const myAlert = document.querySelector('.alert');
+
+        form.addEventListener('submit', e => {
+            // ketika tombol submit di-klik
+            // tampilkan tombol loading
+            btnLoading.classList.toggle('d-none');
+            btnKirim.classList.toggle('d-none');
+
+            e.preventDefault()
+            fetch(scriptURL, {
+                    method: 'POST',
+                    body: new FormData(form)
+                })
+                .then(response => {
+                    btnLoading.classList.toggle('d-none');
+                    btnKirim.classList.toggle('d-none');
+                    // menampilkan alert
+                    myAlert.classList.toggle('d-none');
+
+                    // reset formulirnya
+                    form.reset();
+
+                    console.log('Success!', response)
+
+                })
+                .catch(error => console.error('Error!', error.message))
+        })
+    </script>
 </body>
 
 </html>
